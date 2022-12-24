de703504e3bf8aab69353ea5e5b2d84c3f24d354
Aaron Faanes <dafrito@gmail.com>
2022-12-24 02:59:53 -0800

    wip

--- src/ConvolverWidget.ts
+++ src/ConvolverWidget.ts
@@ -1,9 +1,15 @@
+import generateID from 'parsegraph-generateid';
+import { BlockNode, BlockCaret } from "parsegraph-block";
+import { Projector } from "parsegraph-projector";
+import Direction from "parsegraph-direction";
+import { SliderNode } from "parsegraph-slider";
+
 // https://stackoverflow.com/questions/22525934/connecting-convolvernode-to-an-oscillatornode-with-the-web-audio-the-simple-wa
-export default function impulseResponse(
-  audioContext,
-  duration,
-  decay,
-  reverse
+export function impulseResponse(
+  audioContext:AudioContext,
+  duration:number,
+  decay:number,
+  reverse:boolean
 ) {
   const sampleRate = audioContext.sampleRate;
   const length = sampleRate * duration;
@@ -20,75 +26,92 @@ export default function impulseResponse(
   return impulse;
 }
 
-convolverWidgetCount = 0;
-function ConvolverWidget(graph) {
-  this._id = convolverWidgetCount++;
-  this._graph = graph;
-  this._containerNode = null;
+export default class ConvolverWidget {
+  _id: string;
+  _proj: Projector;
+  _duration: number;
+  _decay: number;
+  _maxDecay: number;
+  _maxDuration: number;
+  _reversed: boolean;
+  _containerNode: BlockNode;
+  _convolver: ConvolverNode;
 
-  this._duration = 0.25;
-  this._decay = 0.25;
+  constructor(proj: Projector) {
+    this._id = generateID("Convolver");
+    this._proj = proj;
+    this._containerNode = null;
 
-  this._maxDuration = 8;
-  this._maxDecay = 8;
-  this._reversed = false;
+    this._duration = 0.25;
+    this._decay = 0.25;
 
-  // const audio = graph.surface().audio();
-  // this._convolver = audio.createConvolver();
-  // this.refresh();
-}
+    this._maxDuration = 8;
+    this._maxDecay = 8;
+    this._reversed = false;
 
-ConvolverWidget.prototype.refresh = function () {
-  const audio = this._graph.surface().audio();
-  if (this._duration == 0 || this._decay == 0) {
-    this._convolver.buffer = null;
-  } else {
-    this._convolver.buffer = impulseResponse(
-      audio,
-      this._duration,
-      this._decay,
-      this._reversed
-    );
+    this._convolver = this.audio().createConvolver();
+    this.refresh();
   }
-};
 
-ConvolverWidget.prototype.node = function () {
-  if (this._containerNode) {
+  audio() {
+    return this._proj.audio();
+  }
+
+  refresh() {
+    const audio = this.audio();
+    if (this._duration == 0 || this._decay == 0) {
+      this._convolver.buffer = null;
+    } else {
+      this._convolver.buffer = impulseResponse(
+        audio,
+        this._duration,
+        this._decay,
+        this._reversed
+      );
+    }
+  };
+
+  node() {
+    if (this._containerNode) {
+      return this._containerNode;
+    }
+    const car = new BlockCaret('b');
+    this._containerNode = car.root();
+    car.label("Convolver");
+
+    car.spawnMove('i', 'u', 'v');
+    car.pull('d');
+    car.shrink();
+    const aSlider = new SliderNode();
+    car.node().connectNode(Direction.DOWNWARD, aSlider);
+    car.spawnMove('f', 'u');
+    car.pull('d');
+    car.shrink();
+    const bSlider = new SliderNode();
+    car.node().connectNode(Direction.DOWNWARD, bSlider);
+
+    aSlider.value().setVal(this._decay / this._maxDecay);
+    aSlider.value().setOnChange(()=>{
+      this._decay = Math.pow(aSlider.value().val(), 2) * this._maxDecay;
+      this.refresh();
+    });
+    bSlider.value().setVal(this._duration / this._maxDuration);
+    bSlider.value().setOnChange(()=>{
+      this._duration = Math.pow(bSlider.value().val(), 2) * this._maxDuration;
+      this.refresh();
+    });
+
+    car.spawnMove('f', 'u');
+    car.pull('d');
+    car.shrink();
+    const reversedButton = car.spawn('d', 's');
+    reversedButton.value().setLabel("Reverse");
+    reversedButton.value().interact().setClickListener(()=>{
+      this._reversed = !this._reversed;
+      this.refresh();
+      return true;
+    });
+
     return this._containerNode;
-  }
-  const car = new Caret(BLOCK);
-  this._containerNode = car.root();
-  car.label("Convolver");
-
-  car.spawnMove(INWARD, BUD, ALIGN_VERTICAL);
-  car.pull(DOWNWARD);
-  car.shrink();
-  const aSlider = car.spawn(DOWNWARD, SLIDER);
-  car.spawnMove(FORWARD, BUD);
-  car.pull(DOWNWARD);
-  car.shrink();
-  const bSlider = car.spawn(DOWNWARD, SLIDER);
-
-  aSlider.setValue(this._decay / this._maxDecay);
-  aSlider.setChangeListener(function () {
-    this._decay = Math.pow(aSlider.value(), 2) * this._maxDecay;
-    this.refresh();
-  }, this);
-  bSlider.setValue(this._duration / this._maxDuration);
-  bSlider.setChangeListener(function () {
-    this._duration = Math.pow(bSlider.value(), 2) * this._maxDuration;
-    this.refresh();
-  }, this);
-
-  car.spawnMove(FORWARD, BUD);
-  car.pull(DOWNWARD);
-  car.shrink();
-  const reversedButton = car.spawn(DOWNWARD, SLOT);
-  reversedButton.setLabel("Reverse", this._graph.font());
-  reversedButton.setClickListener(function () {
-    this._reversed = !this._reversed;
-    this.refresh();
-  }, this);
-
-  return this._containerNode;
-};
+  };
+}
